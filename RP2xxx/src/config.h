dcdee999cf8b03862acbf04ed988e246d9be5dfd
j1rie <j1rie@users.noreply.github.com>
2024-12-06 12:13:25 +0100

    xiao rp2350 pins

--- RP2xxx/src/config.h
+++ RP2xxx/src/config.h
@@ -28,14 +28,14 @@
 
 /* TODO GPIO x Logging (irmp.c) */
 
-#define WAKEUP_GPIO		27 /* GPIO 27 (Pico Pin 32, One+Zero+Seeed XIAO RP2350 Pin 27) */
-#define RESET_GPIO		3  /* GPIO 3  (Pico Pin 5,  One+Zero+Seeed XIAO RP2350 Pin 3)  */
-#define EXTLED_GPIO		4  /* GPIO 4  (Pico Pin 6,  One+Zero+Seeed XIAO RP2350 Pin 4)  */
-#define IR_IN_GPIO		28 /* GPIO 28 (Pico Pin 34, One+Zero+Seeed XIAO RP2350 Pin 28) */
-#define IR_OUT_GPIO		6  /* GPIO 6  (Pico Pin 9,  One+Zero+Seeed XIAO RP2350 Pin 6)  */
-#define WAKEUP_RESET_GPIO	7  /* GPIO 7  (Pico Pin 10, One+Zero+Seeed XIAO RP2350 Pin 7)  */
-#define STATUSLED_GPIO		2  /* GPIO 2  (Pico Pin 4,  One+Zero+Seeed XIAO RP2350 Pin 2)  */
-#define NUMLED_GPIO		5  /* GPIO 5  (Pico Pin 7,  One+Zero+Seeed XIAO RP2350 Pin 5)  */
+#define WAKEUP_GPIO		27 /* GPIO 27 (Pico Pin 32, One+Zero Pin 27, Seeed XIAO RP2350 D1) */
+#define RESET_GPIO		3  /* GPIO 3  (Pico Pin 5,  One+Zero Pin 3, Seeed XIAO RP2350 D10)  */
+#define EXTLED_GPIO		4  /* GPIO 4  (Pico Pin 6,  One+Zero Pin 4, Seeed XIAO RP2350 D9)  */
+#define IR_IN_GPIO		28 /* GPIO 28 (Pico Pin 34, One+Zero Pin 28, Seeed XIAO RP2350 D2) */
+#define IR_OUT_GPIO		6  /* GPIO 6  (Pico Pin 9,  One+Zero Pin 6, Seeed XIAO RP2350 D4)  */
+#define WAKEUP_RESET_GPIO	7  /* GPIO 7  (Pico Pin 10, One+Zero Pin 7, Seeed XIAO RP2350 D5)  */
+#define STATUSLED_GPIO		2  /* GPIO 2  (Pico Pin 4,  One+Zero Pin 2, Seeed XIAO RP2350 D8)  */
+#define NUMLED_GPIO		5  /* GPIO 5  (Pico Pin 7,  One+Zero Pin 5, Seeed XIAO RP2350 D3)  */
 #ifndef SEEED_XIAO_RP2350
 #define IS_RGBW			false
 #else
