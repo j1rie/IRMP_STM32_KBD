2e25fb78214a0bb62dfbdfa7abc83e3ac627c1c1
j1rie <j1rie@users.noreply.github.com>
2025-04-17 12:11:31 +0200

    led table

--- RP2xxx/src/config.h
+++ RP2xxx/src/config.h
@@ -29,13 +29,10 @@
 /* TODO GPIO x Logging (irmp.c) */
 
 #define WAKEUP_GPIO		27 /* GPIO 27 (Pico(2) Pin 32, One+Zero Pin 27, Seeed XIAO RP2350 D1) */
-//#define RESET_GPIO		3  /* GPIO 3  (Pico(2) Pin 5,  One+Zero Pin 3, Seeed XIAO RP2350 D10)  */
 #define EXTLED_GPIO		4  /* GPIO 4  (Pico(2) Pin 6,  One+Zero Pin 4, Seeed XIAO RP2350 D9)  */
 #define IR_IN_GPIO		28 /* GPIO 28 (Pico(2) Pin 34, One+Zero Pin 28, Seeed XIAO RP2350 D2) */
 #define IR_OUT_GPIO		6  /* GPIO 6  (Pico(2) Pin 9,  One+Zero Pin 6, Seeed XIAO RP2350 D4)  */
-//#define WAKEUP_RESET_GPIO	7  /* GPIO 7  (Pico(2) Pin 10, One+Zero Pin 7, Seeed XIAO RP2350 D5)  */
 #define STATUSLED_GPIO		2  /* GPIO 2  (Pico(2) Pin 4,  One+Zero Pin 2, Seeed XIAO RP2350 D8)  */
-//#define NUMLED_GPIO		5  /* GPIO 5  (Pico(2) Pin 7,  One+Zero Pin 5, Seeed XIAO RP2350 D3)  */
 //#ifndef SEEED_XIAO_RP2350
 #define IS_RGBW			false
 //#else
@@ -50,7 +47,7 @@
 /* it seems, pio pins need to be consecutive
  * does this work with USB Erratum RP2040-E5? otherwise choose 17 (but that's only a solder point on Zero and One)
  */
-#define WS2812_PIN_2		WS2812_PIN - 1  /* GPIO 15  (Pico(2) Pin 20, One+Zero Pin 15) */
+#define WS2812_PIN_2		WS2812_PIN - 1  /* GPIO 15 (Pico(2) Pin 20, One+Zero Pin 15), GPIO 21 (Seeed XIAO RP2350 D11) */
 #define NUM_PIXELS		64
 
 #endif /* __CONFIG_H */
