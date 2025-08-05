dc0bf609ffbf2aec26027424fcaf738e9dce42a8
j1rie <j1rie@users.noreply.github.com>
2025-08-05 23:12:20 +0200

    more work on irmphidkbd

--- vdr-plugin-irmphidkbd/irmphidkbd.c
+++ vdr-plugin-irmphidkbd/irmphidkbd.c
@@ -24,12 +24,12 @@ uint8_t debug = 1;
 class cIrmpRemote : public cRemote, private cThread {
 private:
   bool Connect(void);
-  virtual void Action(void) override;
-  bool Stop();
+  void Action(void);
   bool Ready();
   int fd;
 public:
   cIrmpRemote(const char *Name);
+  ~cIrmpRemote();
 };
 
 cIrmpRemote::cIrmpRemote(const char *Name)
@@ -40,30 +40,34 @@ cIrmpRemote::cIrmpRemote(const char *Name)
   Start();
 }
 
+cIrmpRemote::~cIrmpRemote()
+{
+  //ioctl(fd, EVIOCGRAB, 0);
+  int fh = fd;
+  fd = -1;
+  Cancel();
+  if (fh >= 0)
+     close(fh);
+}
+
 bool cIrmpRemote::Connect()
 {
   fd = open(kbd_device, O_RDONLY | O_NONBLOCK);
   if(fd == -1){
-    printf("Cannot open %s. %s.\n", kbd_device, strerror(errno));
-    //return false;
+    if(debug) printf("Cannot open %s. %s.\n", kbd_device, strerror(errno));
+    esyslog("Cannot open %s. %s.\n", kbd_device, strerror(errno));
+    return false;
   } else {
-    printf("opened %s\n", kbd_device);
+    if(debug) printf("opened %s\n", kbd_device);
+    isyslog("opened %s\n", kbd_device);
   }
 
   /*if(ioctl(fd, EVIOCGRAB, 1)){
-    printf("Cannot grab %s. %s.\n", kbd_device, strerror(errno));
+    if(debug) printf("Cannot grab %s. %s.\n", kbd_device, strerror(errno));
   } else {
     if(debug) printf("Grabbed %s!\n", kbd_device);
   }*/
 
-  return true;
-}
-
-bool cIrmpRemote::Stop()
-{
-  //ioctl(fd, EVIOCGRAB, 0);
-  if (fd)
-    close(fd);
   return true;
 }
 
@@ -74,106 +78,123 @@ bool cIrmpRemote::Ready(void)
 
 void cIrmpRemote::Action(void)
 {
-  if(debug) printf("action!\n");
   cTimeMs FirstTime;
   cTimeMs LastTime;
   cTimeMs ThisTime;
+  cTimeMs LoopTime;
   struct input_event event;
   uint8_t magic_key = 173;
   uint8_t only_once = 1;
   bool release_needed = false;
   bool repeat = false;
   bool pair = false;
-  uint16_t timeout = 160, code = 0, code2 = 0, lastcode = 0, lastcode2 = 0;
+  int timeout = -1;
+  int RepeatRate = 118; // greatest repeat rate we know so far (except Sky)
+  uint16_t code = 0, lastcode = 0;
+  //uint16_t code2 = 0, lastcode2 = 0;
+
+  if(debug) printf("irmphidkbd action!\n");
 
   while(Running()){
 
+    //printf("loop timeout: %d, elapsed: %ld\n", timeout, LoopTime.Elapsed());
+    LoopTime.Set();
     bool ready = fd >= 0 && cFile::FileReady(fd, timeout); // implizit mindestens 100 ms!!!
     int ret = ready ? safe_read(fd, &event, sizeof(event)) : -1;
 
     if (fd < 0 || ready && ret <= 0) {
-	esyslog("ERROR: irmphidkbd connection broken, trying to reconnect every %.1f seconds", float(RECONNECTDELAY) / 1000);
-	if (fd >= 0)
-	    close(fd);
-	fd = -1;
-	while (Running() && fd < 0) {
-	    cCondWait::SleepMs(RECONNECTDELAY);
-	    if (Connect()) {
-		isyslog("reconnected to irmphidkbd");
-		break;
-	    }
-	}
+        esyslog("ERROR: irmphidkbd connection broken, trying to reconnect every %.1f seconds", float(RECONNECTDELAY) / 1000);
+        if (fd >= 0)
+            close(fd);
+        fd = -1;
+        while (Running() && fd < 0) {
+            cCondWait::SleepMs(RECONNECTDELAY);
+            if (Connect()) {
+                isyslog("reconnected to irmphidkbd");
+                break;
+            }
+        }
     }
 
-
     if (ready && ret > 0) {
-			if (event.type == EV_KEY && event.value == 1) { // keypress
-				//if(debug) printf("read %ld %d %d --- ", this_time, event.code, pair);
-				pair = (LastTime.Elapsed() < 10)? 1 : 0; // modifier+key, TODO: process modifier // ??
-				if(only_once && event.code == magic_key) {
-					if(debug) printf("magic\n");
-					FILE *out = fopen("/var/log/started_by_IRMP_STM32_KBD", "a");
-					time_t date = time(NULL);
-					struct tm *ts = localtime(&date);
-					fprintf(out, "%s", asctime(ts));
-					fclose(out);
-					only_once = 0;
-				}
+            if (event.type != EV_KEY || event.value != 1) { // unbrauchbar
+                if (timeout >= (int)LoopTime.Elapsed()) {
+                    timeout -= LoopTime.Elapsed(); // aber mind. 25
+                    //if(debug) printf("unusable event, new timeout: %d\n", timeout);
+                }
+                continue;
+            }
+            code = event.code;
+            pair = (FirstTime.Elapsed() < 10)? 1 : 0; // modifier+key, TODO: process modifier
+            if(only_once && code == magic_key) {
+                if(debug) printf("magic\n");
+                FILE *out = fopen("/var/log/started_by_IRMP_STM32_KBD", "a");
+                time_t date = time(NULL);
+                struct tm *ts = localtime(&date);
+                fprintf(out, "%s", asctime(ts));
+                fclose(out);
+                only_once = 0;
+            }
+            if(!pair){
+                int Delta = ThisTime.Elapsed(); // the time between two consecutive events
+                if (debug) printf("Delta: %d\n", Delta);
+                if (RepeatRate > Delta && Delta > 0)
+                    RepeatRate = Delta; // autodetect repeat rate
+                ThisTime.Set();
+                timeout = Setup.RcRepeatTimeout ? Setup.RcRepeatTimeout : RepeatRate * 103 / 100 + 1;  // 3 % + 1 should presumably be enough
+                if (debug) printf("code: %s, lastcode: %s, toggle: %d, timeout: %d, pair: %d\n", evkeys[code], evkeys[lastcode], Setup.RcTogglingProtocol, timeout, pair);
+                if(code != lastcode) { // new key
+                    if (debug) printf("Neuer\n");
+                    if (repeat) {
+                        Put(lastcode, false, true); // generated release for previous repeated key
+                        if (debug) printf("put release for %d\n", lastcode);
+                    }
+                    lastcode = code;
+                    repeat = false;
+                    FirstTime.Set();
+                } else { // repeat
+                    if (debug) printf("Repeat\n");
+                    if (FirstTime.Elapsed() < (uint)Setup.RcRepeatDelay) {
+                        if (debug) printf("continue Delay\n\n");
+                        continue; // repeat function kicks in after a short delay
+                    }
+                    if (LastTime.Elapsed() < (uint)Setup.RcRepeatDelta) {
+                        if (debug)  printf("continue Delta\n\n");
+                        continue; // skip same keys coming in too fast
+                    }
+                    repeat = true;
+                }
+            } else {
+                if (debug) printf("pair!\n");
+            }
 
-				int Delta = ThisTime.Elapsed(); // the time between two consecutive events
-				if (debug) printf("Delta: %d\n", Delta);
-				if(!pair){
-					ThisTime.Set();
-					if(code != lastcode) { // new key // ??
-						if (debug) printf("Neuer\n");
-						if (repeat) {
-							Put(lastcode, false, true); // generated release for previous repeated key
-							printf("put release for %d\n", lastcode);
-							if (pair) {// hier immer falsch!
-								Put(evkeys[lastcode2], false, true);
-							}
-						}
-						if(!pair) { // hier immer richtig!
-							lastcode = event.code;
-						} else {
-							lastcode2 = event.code;
-						}
-						repeat = false;
-						FirstTime.Set();
-					} else { // repeat
-						if (debug) printf("Repeat\n");
-						if (FirstTime.Elapsed() < (uint)Setup.RcRepeatDelay || LastTime.Elapsed() < (uint)Setup.RcRepeatDelta) {
-							if (debug) printf("continue\n\n");
-							continue; // don't send key
-						}
-						repeat = true;
-						}
-					}
-
-				/* send key */
-					if(!pair) {
-						code = event.code;
-					} else {
-						code2 = event.code;
-					}
-					if(debug) printf("delta send: %ld\n", LastTime.Elapsed());
-					LastTime.Set();
-					Put(evkeys[event.code], repeat);
-					release_needed = true;
-			}
-		}
-
-		/* send release */
-		if (ThisTime.Elapsed() > timeout && release_needed && repeat) {
-			release_needed = false;
-			if(debug) printf("delta release: %ld timeout: %d code: %s\n", ThisTime.Elapsed(), timeout, evkeys[lastcode]);
-			cRemote::Put(evkeys[lastcode], false, true);
-			if (pair) {
-			if(debug) printf("delta release: %ld timeout: %d code: %s\n", ThisTime.Elapsed(), timeout, evkeys[lastcode2]);
-				Put(evkeys[lastcode2], false, true);
-			}
-		}
-	}
+            /* send key */
+            if(!pair) {
+                //code = code;
+            } else {
+                //code2 = code;
+            }
+            if(debug) printf("delta send: %ld\n", LastTime.Elapsed());
+            LastTime.Set();
+            Put(evkeys[code], repeat);
+            release_needed = true;
+    } else { // no key within timeout
+            if (release_needed && repeat) {
+                if(debug) printf("put release for %s, delta %ld\n", evkeys[lastcode], ThisTime.Elapsed());
+                Put(evkeys[lastcode], false, true);
+                /*if (pair) {
+                    if(debug) printf("put release for %s, delta %ld\n", evkeys[lastcode2], ThisTime.Elapsed());
+                    Put(evkeys[lastcode2], false, true);
+                }*/
+            }
+            release_needed = false;
+            repeat = false;
+            lastcode = 0;
+            timeout = -1;
+            if (debug) printf("reset\n");
+    }
+    if (debug) printf("\n");
+  }
 }
 
 class cPluginIrmphidkbd : public cPlugin {
